ff93fdafa456b5d660a4a7fc308a3070cae9306b
Ch. Muhammad Saad <chsaad0911@gmail.com>
2026-09-10 22:23:59 +0500

    Sprint 4 day 2: first ObservableProperty, verified generator output

---
+++ ViewModels/ProductsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PosApp.ViewModels
+{
+    public partial class ProductsViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool _isBusy;
+    }
+}
